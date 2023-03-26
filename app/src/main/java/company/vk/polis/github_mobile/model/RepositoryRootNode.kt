8ca1f774bc4a9b75e271de85dd1f1d1420f3e596
StombieIT <stombie@yandex.ru>
2023-03-26 14:37:56 +0300

    Реализована логика осуществления запроса узлов (node) репозитория

--- app/src/main/java/company/vk/polis/github_mobile/model/RepositoryRootNode.kt
+++ app/src/main/java/company/vk/polis/github_mobile/model/RepositoryRootNode.kt
@@ -3,7 +3,7 @@ package company.vk.polis.github_mobile.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class TreeNode(
+data class RepositoryRootNode(
     val sha: String,
     val tree: List<Node>
 )
--- app/src/main/java/company/vk/polis/github_mobile/remote/GitHubApi.kt
+++ app/src/main/java/company/vk/polis/github_mobile/remote/GitHubApi.kt
@@ -1,13 +1,22 @@
 package company.vk.polis.github_mobile.remote
 
 import company.vk.polis.github_mobile.model.Repository
+import company.vk.polis.github_mobile.model.RepositoryRootNode
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 
 interface GitHubApi {
     @GET("user/repos")
     fun getUserRepositories(
         @Header("Authorization") token: String
     ): Call<List<Repository>>
+
+    @GET("repos/{owner}/{repo}/git/trees/{branch}?recursive=1")
+    fun getRepositoryRootNode(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String,
+        @Path("branch") branch: String
+    ): Call<RepositoryRootNode>
 }
