191ddd6454ab97cc80e7a7952c509a168aa1c58e
StombieIT <stombie@yandex.ru>
2023-04-02 21:53:01 +0300

    Partly implement list of repositories

---
+++ app/src/main/java/company/vk/polis/github_mobile/ui/repositories_list/RepositoriesFragment.kt
@@ -0,0 +1,45 @@
+package company.vk.polis.github_mobile.ui.repositories_list
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import company.vk.polis.github_mobile.R
+import company.vk.polis.github_mobile.model.Repository
+import company.vk.polis.github_mobile.remote.ApiClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class RepositoriesFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_repositories, container, false)
+
+        recyclerView = view.findViewById<RecyclerView>(R.id.repositories)
+        recyclerView.layoutManager = LinearLayoutManager(container!!.context)
+        recyclerView.adapter = RepositoriesListAdapter(emptyList())
+
+        ApiClient.gitHubApi.getUserRepositories("Bearer ${ApiClient.token}").enqueue(object :
+            Callback<List<Repository>> {
+            override fun onResponse(
+                call: Call<List<Repository>>,
+                response: Response<List<Repository>>
+            ) {
+                recyclerView.adapter = RepositoriesListAdapter(response.body()!!)
+            }
+
+            override fun onFailure(call: Call<List<Repository>>, t: Throwable) {
+                Log.e("repo_fragment_get_data_error", t.stackTraceToString())
+            }
+        })
+
+        return view
+    }
+}
