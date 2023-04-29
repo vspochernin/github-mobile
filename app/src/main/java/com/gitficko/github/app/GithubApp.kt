a46db68a085f86d7738ebd5c5d9614761b247df5
Ekaterina Zelenkina <katrinzel0@mail.ru>
2023-04-29 23:11:16 +0300

    ui sign in screen done

--- app/src/main/java/com/gitficko/github/app/GithubApp.kt
+++ app/src/main/java/com/gitficko/github/app/GithubApp.kt
@@ -1,6 +1,9 @@
 package com.gitficko.github.app
 
 import android.app.Application
+import android.os.Build
+import android.os.Bundle
+import android.view.View
 import com.gitficko.github.remote.Networking
 import timber.log.Timber
 
