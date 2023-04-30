cdca7f8cf7578b1c99f652d7e7f88226a0114552
Vladislav Pochernin <vspochernin@gmail.com>
2023-04-30 23:54:25 +0300

    implement bottom navigation

--- app/src/main/java/com/gitficko/github/MainActivity.kt
+++ app/src/main/java/com/gitficko/github/MainActivity.kt
@@ -1,7 +1,38 @@
 package com.gitficko.github
 
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+
+        // Настройка BottomNavigationView с помощью NavigationUI.
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+        // Скрытие BottomNavigationView для фрагментов, кроме home и profile.
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.navigation_home,
+                R.id.navigation_profile -> bottomNavigationView.visibility = View.VISIBLE
+                else -> bottomNavigationView.visibility = View.GONE
+            }
+        }
+    }
 
 }
