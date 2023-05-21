dc150e4f0b0ee04a1d5bda917aa116a27a69f6f4
Vladislav Yartsev <vladislav.yartsev@vk.team>
2023-05-21 11:59:41 +0300

    Переписал пулл реквесты

---
+++ app/src/main/java/com/gitficko/github/ui/QueryableListViewModel.kt
@@ -0,0 +1,23 @@
+package com.gitficko.github.ui
+
+import androidx.lifecycle.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import java.util.stream.Collectors
+
+abstract class QueryableListViewModel<T> : ViewModel() {
+    protected val list = MutableStateFlow<List<T>>(emptyList())
+    protected val query = MutableStateFlow("")
+
+    val suitableList = combine(list, query) { list, query ->
+        list.stream().filter { item ->
+            validateItem(item, query)
+        }.collect(Collectors.toList())
+    }
+
+    fun updateQuery(query: String) {
+        this.query.value = query
+    }
+
+    abstract fun validateItem(item: T, query: String): Boolean;
+}
---
+++ app/src/main/java/com/gitficko/github/ui/pull_requests_list/PullRequestsListViewModel.kt
@@ -0,0 +1,34 @@
+package com.gitficko.github.ui.pull_requests_list
+
+import androidx.lifecycle.viewModelScope
+import com.gitficko.github.model.PullRequest
+import com.gitficko.github.remote.CachedClient
+import com.gitficko.github.ui.QueryableListViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class PullRequestsListViewModel : QueryableListViewModel<PullRequest>() {
+
+    fun loadPullRequestsByOwnerLogin(ownerLogin: String) {
+        viewModelScope.launch {
+            try {
+                val result = withContext(Dispatchers.IO) {
+                    CachedClient.getPullRequestsOf(ownerLogin)
+                }
+
+                withContext(Dispatchers.Main) {
+                    list.value = result
+                }
+            } catch (e: Exception) {
+                list.value = emptyList()
+            } finally {
+                query.value = ""
+            }
+        }
+    }
+
+    override fun validateItem(item: PullRequest, query: String): Boolean {
+        return item.title.startsWith(query)
+    }
+}
